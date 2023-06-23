3e9f2d38f456cf14078db763f89d87745e17023f
mat-shepherd <>
2023-06-23 16:57:57 +0000

    Feat: add screen render methods, screen data files, and main function

--- run.py
+++ run.py
@@ -16,43 +16,106 @@ from colorama import Fore
 init(autoreset=True)
 
 # Classes
+
+
 class Player:
+    """
+    The Player class to contain all player attributes
+    """
     def __init__(self,
-                 name,
-                 score,
-                 high_score,
-                 round_time,
-                 current_round,
-                 chosen_letters,
-                 chosen_numbers):
+                 name='Player',
+                 score=None,
+                 high_score=None,
+                 round_time=None,
+                 current_round=None,
+                 chosen_letters=None,
+                 chosen_numbers=None):
         self.name = name
         self.score = score
         self.high_score = high_score
         self.round_time = round_time
         self.current_round = current_round
         self.chosen_letters = chosen_letters
-        self.chosen_numbers = chosen_numbers        
+        self.chosen_numbers = chosen_numbers
 
 class Screen:
-    def __init__(self, name, content):
-        self.name = name
-        self.content = content
+    """
+    The Screen class to contain all screen
+    definitions and attributes
+    """
+    def __init__(self, screen_data_file):
+        self.screen_data_file = screen_data_file
+
+    def render(self):
+        # Render screen elements in the terminal
+        self.display_text()
+        self.display_prompt()
+
+    def display_text(self):
+        # Retrieve screen text from data files
+        try:
+            with open(self.screen_data_file) as f:
+                text = f.read()
+                print(text)
+        except OSError as e:
+            errno, strerror = e.args
+            print(f'There is an I/O error number, {errno}: {strerror}.')
+
+    def display_prompt(self):
+        # Display relevant screen prompt
+        if self.screen_data_file == 'intro_screen_data.txt':
+            user_prompt = input(
+                'Enter 1 to start the game or 2'
+                ' to read the game rules\n'
+                )
+        elif self.screen_data_file == 'rules_screen_data.txt':
+            user_prompt = input(
+                'Enter 1 to start the game or 2 to return to the intro screen\n'
+                )
+        elif self.screen_data_file == 'start_game_screen_data.txt':
+            user_prompt = input('Please Enter Your Name\n')
+
 
 class Letters:
+    """
+    The Letters class to contain all letters
+    to choose from in the letters round
+    """
     def __init__(self, vowels, consonants):
         self.vowels = vowels
         self.consonants = consonants
 
+
 class Numbers:
+    """
+    The Numbers class to contain all numbers
+    to choose from in the numbers round
+    """
     def __init__(self, big, small, target):
         self.big = big
         self.small = small
         self.target = target
 
+
 class Conundrum:
+    """
+    The Conundrum class to contain the
+    Conundrum word attributes for the
+    Conundrum round
+    """
     def __init__(self, target, scrambled):
         self.target = target
-        self.scrambled = scrambled 
+        self.scrambled = scrambled
 
-# Game
 
+# Main game functions
+def main():
+    """
+    Run all program functions
+    """
+    intro_screen = Screen("intro_screen_data.txt")
+    intro_screen.render()
+    # Player(name,score,high_score,round_time,current_round,chosen_letters,chosen_numbers)
+
+# Call main game function
+main()

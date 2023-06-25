bfa633acde5392eb06aa92f935a0fdf24d8e14e0
mat-shepherd <>
2023-06-25 09:18:43 +0000

    Feat: add validation to game start inputs

--- run.py
+++ run.py
@@ -50,29 +50,29 @@ class Screen:
     def __init__(self, screen_data_file):
         self.screen_data_file = screen_data_file
 
-    def render(self, fore_colour, back_colour):
+    def render(self):
         # Set screen bg to blue and render screen elements in the terminal
         print(Back.BLUE)
         print(clear_screen())
-        self.display_art_text(fore_colour, back_colour)
-        self.display_text(fore_colour, back_colour)
-        user_prompt = self.display_prompt(fore_colour, back_colour)
+        self.display_art_text()
+        self.display_text()
+        user_prompt = self.display_prompt()
 
         return user_prompt
 
-    def display_art_text(self, fore_colour, back_colour):
+    def display_art_text(self):
         # Output text as ASCII art via Figlet library
         if self.screen_data_file == 'intro_screen_data.txt':
             result = text2art("        COUNTDOWN", font='small')
-            print(fore_colour + back_colour + result)
+            print(result)
         elif self.screen_data_file == 'rules_screen_data.txt':
             result = text2art("        COUNTDOWN RULES", font='small')
-            print(fore_colour + back_colour + result)
+            print(result)
         elif self.screen_data_file == 'start_game_screen_data.txt':
             result = text2art("        ROUND ONE", font='small')
-            print(fore_colour + back_colour + result)
+            print(result)
 
-    def display_text(self, fore_colour, back_colour):
+    def display_text(self):
         # Retrieve screen text from data files
         try:
             with open(self.screen_data_file) as f:
@@ -82,20 +82,42 @@ class Screen:
             errno, strerror = e.args
             print(f'There is an I/O error number, {errno}: {strerror}.')
 
-    def display_prompt(self, fore_colour, back_colour):
+    def display_prompt(self):
         # Display relevant screen prompt
         if self.screen_data_file == 'intro_screen_data.txt':
-            user_prompt = input(
-                'Enter 1 to Start the Game or 2'
-                ' to Read the Game Rules\n'
-                )
+            try:
+                user_prompt = input(
+                    'Enter 1 to Start the Game or 2'
+                    ' to Read the Game Rules\n'
+                    )
+                if not (1 <= int(user_prompt) <= 2):
+                    raise ValueError
+            except ValueError:
+                print(Fore.RED + 'Please enter only 1 or 2')
         elif self.screen_data_file == 'rules_screen_data.txt':
-            user_prompt = input(
-                'Enter 1 to Start the Game or 2'
-                ' to Return to the Intro Screen\n'
-                )
+            try:
+                user_prompt = input(
+                    'Enter 1 to Start the Game or 2'
+                    ' to Return to the Intro Screen\n'
+                    )
+                if not (1 <= int(user_prompt) <= 2):
+                    raise ValueError
+            except ValueError:
+                print(Fore.RED + 'Please enter only 1 or 2')
         elif self.screen_data_file == 'start_game_screen_data.txt':
-            user_prompt = input('Please Enter Your Name\n')
+            while True:
+                try:
+                    user_prompt = input('Please Enter Your Name\n')
+                    if (len(user_prompt) > 2 AND user_prompt.isalpha()):
+                    break
+                else:
+                    raise Exception
+                except TypeError:
+                    print(Fore.RED + 'Please enter letters only')
+                    continue
+                except EOFError:
+                    print(Fore.RED + 'Please enter your name')
+                    continue
 
         return user_prompt
 
@@ -138,7 +160,7 @@ def main():
     Run all program functions
     """
     intro_screen = Screen('intro_screen_data.txt')
-    user_response = intro_screen.render(Fore.BLACK, Fore.WHITE)
+    user_response = intro_screen.render()
     print(user_response)
     # Player(name,score,high_score,round_time,current_round,chosen_letters,chosen_numbers)
 

12e8e22476dba4ad477c0c23ccb9ab404f35dbec
mat-shepherd <>
2023-07-09 15:38:10 +0000

    Test: add additional name validation tests

--- test_validation.py
+++ test_validation.py
@@ -4,7 +4,21 @@
 import unittest
 # Internal
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-import validation
+from validation import (
+    validate_name,
+    validate_menu_value,
+    validate_vowels,
+    check_profanity,
+    check_dictionary,
+    print_word_meaning,
+    check_letters_used,
+    validate_numbers,
+    validate_user_word,
+    check_numbers_used,
+    validate_user_numbers,
+    validate_user_solution,
+    validate_user_conundrum
+)
 
 
 class TestValidation(unittest.TestCase):
@@ -13,8 +27,20 @@ class TestValidation(unittest.TestCase):
         """
         Tests if validate_name function returns expected values.
         """
-        self.assertTrue(validation.validate_name("Matthew"))
-        self.assertFalse(validation.validate_name("M"))
+        self.assertTrue(validate_name("Matthew"))
+        self.assertTrue(validate_name("Will"))
+        self.assertTrue(validate_name("MS"))
+        self.assertTrue(validate_name("MatShep"))
+        self.assertFalse(validate_name("M"))
+        self.assertFalse(validate_name("123"))
+        self.assertFalse(validate_name("@#'"))
+        self.assertFalse(validate_name("Mat123"))
+        self.assertFalse(validate_name(""))
+        self.assertFalse(validate_name(" Mat"))
+        self.assertFalse(validate_name("   "))
+        self.assertFalse(validate_name("Mat Shep"))
+        self.assertFalse(validate_name("M@t"))
+        self.assertFalse(validate_name("MatShepherd"))
 
 
 if __name__ == '__main__':
